f4df60cc58d1f7bfbc0f99ba8e6abf5c3d8e204a
Matthew <mbrimmer1@gmail.com>
2016-08-22 21:21:30 -0400

    Simple stopwatch

--- stopwatch2.js
+++ stopwatch2.js
@@ -1,3 +1,38 @@
-var date =  new Date();
-var timestamp = date.getTime();
-console.log(timestamp);
+
+function StopWatch() {
+  var startTime = null;
+  var stopTime = null;
+  var running = false;
+
+  this.start = function() {
+    if (running === true) {
+        return;
+    } else if (startTime !== null) {
+      stopTime = null;
+    }
+  };
+
+  this.stop = function() {
+    if (running === false) {
+      return;
+    }
+    stopTime = getTime();
+    running = false;
+  };
+
+  this.duration = function() {
+    if (startTime === null || stopTime === null) {
+      return undefined;
+    } else {
+      return (stopTime - startTime) / 1000;
+    }
+  };
+}
+
+function getTime() {
+  var date =  new Date();
+  return date.getTime();
+}
+
+var stopwatch = new StopWatch();
+console.log(stopwatch);
